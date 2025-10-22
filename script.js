e5c347e616ecfcf69bbd4f206cb8f11ee1e3a09a
escuridao9 <123279814+Escuridao9@users.noreply.github.com>
2025-10-22 11:22:03 +0100

    Add a book manually and make a constructor

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const myLibrary = [];
 
-function Book() {
+function Book(id, author, title, pages, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
 
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary() {
